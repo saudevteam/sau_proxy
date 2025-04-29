2aa9b2862e48d2539750938bd5348725aba07102
DevLothen <52202168+DevLothen@users.noreply.github.com>
2025-04-29 23:36:34 +0200

    2.10.0

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+  name: "Tun2SocksKit",
+  products: [
+    .library(
+        name: "Tun2SocksKit",
+        targets: ["Tun2SocksKit"]
+    ),
+    .library(
+        name: "Tun2SocksKitC",
+        targets: ["Tun2SocksKitC"]
+    )
+  ],
+  targets: [
+    .target(
+        name: "Tun2SocksKit",
+        dependencies: ["HevSocks5Tunnel", "Tun2SocksKitC"]
+    ),
+    .target(
+        name: "Tun2SocksKitC",
+        publicHeadersPath: "."
+    ),
+    .binaryTarget(
+        name: "HevSocks5Tunnel",
+        url: "https://github.com/saudevteam/sau_proxy/releases/download/2.10.0/HevSocks5Tunnel.xcframework.zip",
+        checksum: "ac28f486ec9f6504c2059277df3775651a77341f3c565745fd65480e80c8f3af"
+    )
+  ]
+)
